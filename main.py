880c7e7b2c570caac4fcc20e06c77332940ee80b
yuuns <yuuns1@hotmail.com>
2022-10-12 10:27:17 +0300

    add menu

---
+++ main.py
@@ -0,0 +1,77 @@
+from PyQt5 import QtCore, QtGui, QtWidgets,QtWidgets
+from ui_form import (Ui_mainWindow)
+import os
+
+os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
+def handleVisibleChanged():
+    if not QtGui.QGuiApplication.inputMethod().isVisible():
+        return
+    for w in QtGui.QGuiApplication.allWindows():
+        if w.metaObject().className() == "QtVirtualKeyboard::InputView":
+            keyboard = w.findChild(QtCore.QObject, "keyboard")
+            if keyboard is not None:
+                r = w.geometry()
+                r.moveTop(keyboard.property("y"))
+                w.setMask(QtGui.QRegion(r))
+                return
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    MainWindow.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.MSWindowsFixedSizeDialogHint)
+    ui = Ui_mainWindow()
+    ui.setupUi(MainWindow)
+
+    ## Global Functions connections #########################################################################
+    QtGui.QGuiApplication.inputMethod().visibleChanged.connect(handleVisibleChanged)
+
+
+
+
+    ## Home Page event ###############################################################################
+    ui.homeButton.clicked.connect(lambda: goPage(ui.homePage))
+    ui.userButton.clicked.connect(lambda: goPage(ui.loginPage))
+    ui.settingsButton.clicked.connect(lambda: goPage(ui.settingPage))
+    ui.listButton.clicked.connect(lambda: goPage(ui.listPage))
+    ui.graphButton.clicked.connect(lambda: goPage(ui.graphPage))
+
+
+    ## Login page event ##############################################################################
+    ui.loginButton.clicked.connect(lambda: goPage(ui.homePage))
+    ui.newUserButton.clicked.connect(lambda: goPage(ui.homePage))
+
+    ## Settings page event ###########################################################################
+
+
+    ## List page event ###############################################################################
+
+
+    ## graph page event ##############################################################################
+
+
+
+
+    ## Main Functions  ####################################################################################
+    def goPage(page):
+        print (str(page.text) + "Gecildi")
+        ui.stackedWidget.setCurrentWidget(page)
+
+
+
+
+    ##################################################################################################
+
+
+
+    #MainWindow.showFullScreen() 
+    MainWindow.show() 
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    import sys
+    main()
+
+
